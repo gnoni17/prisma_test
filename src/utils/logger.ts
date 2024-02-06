d6c2df4baf9040e8f8fe4a002cc6505cf91035c9
Gabriele Gnoni <dev02@rizzellosrl.it>
2024-02-06 12:58:22 +0100

    feat: log file

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,3 +1,5 @@
+import fs from 'fs'
+
 interface ILogger {
   info: (message: string) => void;
   warn: (message: string) => void;
@@ -5,29 +7,39 @@ interface ILogger {
   error: (message: string) => void;
 }
 
+function createLogFile(message: string) {
+  const date = new Date()
+  const day = date.getDate()
+  const month = date.getMonth()
+  const year = date.getFullYear()
+  const createStream = fs.createWriteStream(`src/log/${day}-${month}-${year}.txt`)
+  createStream.write(message)
+  createStream.end()
+}
+
 const developmentLogger = (): ILogger => ({
   info: (message: string) => {
-    console.log(message);
+    console.info(message);
   },
   warn: (message: string) => {
-    console.log(message);
+    console.warn(message);
   },
   debug: (message: string) => {
-    console.log(message);
+    console.debug(message);
   },
   error: (message: string) => {
-    console.log(message);
+    console.error(message);
   },
 });
 
 const productionLogger = (): ILogger => ({
   info: (message: string) => {},
   warn: (message: string) => {
-    console.log(message);
+    createLogFile(message)
   },
   debug: (message: string) => {},
   error: (message: string) => {
-    console.log(message);
+    createLogFile(message)
   },
 });
 
